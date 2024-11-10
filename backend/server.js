fe3fb37ffe7a42a3fea69bf46782d1950d8f2806
Tarun Tailor <taruntailor7@gmail.com>
2024-11-10 16:18:26 +0530

    feat: flag added for answered questoin so that for all users question will be changed

--- backend/server.js
+++ backend/server.js
@@ -3,19 +3,31 @@ const http = require('http');
 const { Server } = require('socket.io');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const cors = require('cors');
 dotenv.config();
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-const port = process.env.PORT;
+app.use(cors({
+    origin: '*', // Allow all origins
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true  // If you need to allow cookies (optional)
+}));
+
+const port = process.env.PORT || 5000;
 const mongoUrl = process.env.MONGODB_URI;
 console.log('port',port)
 console.log('mongoUrl', mongoUrl);
 
 // Connect to MongoDB for high-score tracking
-mongoose.connect(mongoUrl);
+mongoose.connect(mongoUrl).then(() => {
+    console.log('Connected to MongoDB');
+}).catch((error) => console.log('MongoDB connection error:', error));
+
+let currentQuestion = generateQuestion();
+let isAnswered = false;
 
 // Question generator function
 function generateQuestion() {
@@ -28,16 +40,26 @@ function generateQuestion() {
 io.on('connection', (socket) => {
     console.log('User connected:', socket.id);
 
-    // Send new question
-    const question = generateQuestion();
-    io.emit('newQuestion', question);
+    // Emit the initial question to the newly connected user
+    socket.emit('newQuestion', { question: currentQuestion.question });
 
-    // Handle answer submission
     socket.on('submitAnswer', (data) => {
-        if (data.answer == question.answer) {
-            io.emit('winner', { user: socket.id, answer: data.answer });
-            question = generateQuestion(); // Generate a new question
-            io.emit('newQuestion', question); // Broadcast new question
+        if (isAnswered) {
+            return; // Ignore answers if the question has already been answered
+        }
+
+        if (data.answer === currentQuestion.answer) {
+            isAnswered = true; 
+            io.emit('winner', { user: socket.id }); // Broadcast winner message
+
+            // Delay sending a new question by 3 seconds
+            setTimeout(() => {
+                currentQuestion = generateQuestion();
+                isAnswered = false;
+                io.emit('newQuestion', { question: currentQuestion.question });
+            }, 3000);
+        } else {
+            socket.emit('incorrect'); // Notify only the user who answered wrong
         }
     });
 
@@ -47,5 +69,5 @@ io.on('connection', (socket) => {
 });
 
 server.listen(port, () => {
-    console.log('Server running on port: ', port);
+    console.log('Server running on port:', port);
 });
