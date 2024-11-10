813ad514ff24422928703ee9eef1d782a8af9706
Tarun Tailor <taruntailor7@gmail.com>
2024-11-10 16:42:39 +0530

    feat: backend deployed over render and deployed url added in frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import io from 'socket.io-client';
 
-const socket = io('http://localhost:5000');
+const socket = io('https://math-quiz-website.onrender.com');
 
 function App() {
     const [question, setQuestion] = useState('');
